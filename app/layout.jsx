81b5dac9034c9db55b7f68505c97f4e41af7364d
Oluwafunmilayo Dada <oluwafunmilayodada2018@gmail.com>
2023-12-03 00:43:30 +0100

    nav bar created

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+import Navbar from "./components/navbar/Navbar";
+
+const montserrat = Montserrat({
+	subsets: ["latin"],
+});
+
+export const metadata = {
+	title: "Funmi Dada Portfolio",
+	description:
+		"a portfolio webpage that showcases my skills has Frontend Developer",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body className={montserrat.className}>
+				<Navbar />
+				{children}
+			</body>
+		</html>
+	);
+}
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className="bg-light w-full min-h-screen">
+			<h1>Home</h1>
+		</main>
+	);
+}
